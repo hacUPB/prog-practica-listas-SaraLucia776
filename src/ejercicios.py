9fbd48afcc70060241900c9366149dc23491b482
Sara Lucia Jara <santiago.londonos@upb.edu.com>
2024-09-24 09:37:50 -0500

    Ejercicio 1

--- src/ejercicios.py
+++ src/ejercicios.py
@@ -1,10 +1,14 @@
 # Ejercicio 1: Suma de elementos en una lista de listas
-def suma_matriz(matriz):
+def suma_matriz(matriz:list):
     """
     Recibe una lista de listas y devuelve la suma de todos sus elementos.
     Incluir el código aquí para sumar los elementos de la matriz.
     """
-    pass
+    suma = 0 
+    for filas in matriz:
+        for dato in filas:
+            suma += dato 
+    return suma 
 
 # Ejercicio 2: Encontrar el valor máximo en una matriz
 def maximo_matriz(matriz):
@@ -12,7 +16,7 @@ def maximo_matriz(matriz):
     Recibe una lista de listas y devuelve el valor máximo.
     Incluir el código aquí para encontrar el valor máximo en la matriz.
     """
-    pass
+    
 
 # Ejercicio 3: Verificar si un número es primo
 def es_primo(n):
